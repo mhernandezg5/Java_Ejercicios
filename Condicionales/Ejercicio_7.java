1580f7bc528e72c9ef2d13aa1b7ab139f846133b
myn0r <myn0r@hotmail.com>
2023-11-08 11:53:10 -0600

    Create Ejercicio_7.java

---
+++ Condicionales/Ejercicio_7.java
@@ -0,0 +1,35 @@
+
+/**
+ * Pedir tres números ym ostrarlos de mayor a menor
+ * 1
+ * 5
+ * 9
+ */
+import javax.swing.JOptionPane;
+
+public class Ejercicio_7 {
+    public static void main(String[] args) {
+        int numero_1, numero_2, numero_3;
+
+        numero_1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primero número"));
+        numero_2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número"));
+        numero_3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tercer número"));
+
+        if (numero_1 > numero_2) {
+            int temp = numero_1;
+            numero_1 = numero_2;
+            numero_2 = temp;
+        } else if (numero_2 > numero_3) {
+            int temp = numero_2;
+            numero_2 = numero_3;
+            numero_3 = temp;
+        } else if (numero_1 > numero_2) {
+            int temp = numero_1;
+            numero_1 = numero_2;
+            numero_2 = temp;
+        }
+        JOptionPane.showMessageDialog(null,
+                "Los números ordenados de mayor a menor son: " + numero_3 + ", " + numero_2 + ", " + numero_1);
+    }
+
+}
