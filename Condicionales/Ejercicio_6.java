b12f91b805d36aab882bcd90cd6176e9a075759d
myn0r <myn0r@hotmail.com>
2023-11-08 11:22:46 -0600

    je

--- Condicionales/Ejercicio_6.java
+++ Condicionales/Ejercicio_6.java
@@ -12,9 +12,9 @@ public class Ejercicio_6 {
         numero_1 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el primer número"));
         numero_2 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el segundo número"));
 
-        if (numero_1 % 2 == 0 && numero_2 % 2 == 0) {
+        if ((numero_1 % 2 == 0) && (numero_2 % 2 == 0)) {
             JOptionPane.showMessageDialog(null, "Ambos son pares");
-        } else if (numero_1 % 2 != 0 && numero_2 != 0) {
+        } else if ((numero_1 % 2 != 0) && (numero_2 != 0)) {
             JOptionPane.showMessageDialog(null, "No son pares");
         } else {
             JOptionPane.showMessageDialog(null, "Uno es par y otro impar");
