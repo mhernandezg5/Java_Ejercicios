2b7a03129c227c3e3ca4b4a1c6ea2b74f281fa16
myn0r <myn0r@hotmail.com>
2023-11-09 11:35:17 -0600

    dia mes
    comparacion

--- Condicionales/Ejercicio_9.java
+++ Condicionales/Ejercicio_9.java
@@ -14,7 +14,7 @@ public class Ejercicio_9 {
         int mes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el mes"));
         int anio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año"));
 
-        if (dia >= 1 && mes <= 12 && anio > 0) {
+        if ((dia >= 1 && dia <= 30) && (mes >= 1 && mes <= 12) && (anio > 0)) {
             JOptionPane.showMessageDialog(null, "Fecha correcta  " + dia + " - " + mes + " - " + anio);
         } else {
             JOptionPane.showMessageDialog(null, "Beeeeeeeep fecha incorrecta");
