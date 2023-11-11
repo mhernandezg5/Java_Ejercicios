d3b2effceb5cd9cb851484a53878bd2f26dc4f04
myn0r <myn0r@hotmail.com>
2023-11-10 19:45:39 -0600

    Operadores

--- Ciclos/Ejercicio_1.java
+++ Ciclos/Ejercicio_1.java
@@ -11,7 +11,11 @@ public class Ejercicio_1 {
             // La potencia devuelve un tipo double entonces init trunca ese valor y dal
             // entero
             resultado = (int) Math.pow(numero, 2);
+
+            // Mujestra el valor con su potencia
             JOptionPane.showMessageDialog(null, "La potencia de " + numero + " es " + resultado);
+
+            // Pide ingresar otro número
             numero = Integer.parseInt(JOptionPane.showInputDialog("Escribre otro número"));
         }
     }
