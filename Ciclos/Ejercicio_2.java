5d6fbd778a92173e1cc7b14ab5dfdc9cc24a8b12
myn0r <myn0r@hotmail.com>
2023-11-13 16:25:40 -0600

    Ejercicio 2

--- Ciclos/Ejercicio_2.java
+++ Ciclos/Ejercicio_2.java
@@ -8,14 +8,17 @@ import javax.swing.JOptionPane;;
 
 public class Ejercicio_2 {
     public static void main(String[] args) {
-        int numero = Integer.parseInt(JOptionPane.showInputDialog("Escribe el número"));
+        int numero;
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Escribe el número"));
+
         while (numero != 0) {
             if (numero > 0) {
                 JOptionPane.showMessageDialog(null, "El número " + numero + " es positivo");
-            } else if (numero < 0) {
+            } else {
                 JOptionPane.showMessageDialog(null, "El número " + numero + " es negativo");
             }
             numero = Integer.parseInt(JOptionPane.showInputDialog("Escribe el número"));
+
         }
     }
 }
