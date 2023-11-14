ef224e179b5a2d3920438089a2bde4b9ac139c03
myn0r <myn0r@hotmail.com>
2023-11-14 11:27:34 -0600

    Ejercicios

--- Ciclos/Ejercicio_4.java
+++ Ciclos/Ejercicio_4.java
@@ -4,8 +4,18 @@
  */
 package Ciclos;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio_4 {
     public static void main(String[] args) {
+        int contador = 0;
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número"));
+        while (numero >= 0) {
+            contador++;
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número"));
+            System.out.println(contador);
+        }
 
+        JOptionPane.showMessageDialog(null, "Se han introducido " + contador + "veces");
     }
 }
