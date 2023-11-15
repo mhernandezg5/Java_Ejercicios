00f63be856755357e400e475faa172f721a1af23
unknown <myn0r>
2023-11-15 22:10:29 +0100

    Ejercicio 12

---
+++ Ciclos/Ejercicio_12.java
@@ -0,0 +1,25 @@
+/*
+ * Pedir un número y calcular
+ * su factorial
+ */
+package Ciclos;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio_12 {
+
+  public static void main(String[] args) {
+    int factorial = 1;
+    int numero = Integer.parseInt(
+      JOptionPane.showInputDialog("Ingrese un número")
+    );
+    for (int contador = 1; contador <= numero; contador++) {
+      System.out.println(contador);
+      factorial *= contador;
+    }
+    JOptionPane.showMessageDialog(
+      null,
+      "El factorial de " + numero + " es " + factorial
+    );
+  }
+}
