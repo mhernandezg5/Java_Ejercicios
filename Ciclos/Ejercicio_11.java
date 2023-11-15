78f12429e04b550420575a4d5b5c9266b0dff1bc
unknown <myn0r>
2023-11-15 22:03:12 +0100

    Ejercicio 11

--- Ciclos/Ejercicio_11.java
+++ Ciclos/Ejercicio_11.java
@@ -7,13 +7,13 @@ package Ciclos;
 public class Ejercicio_11 {
 
   public static void main(String[] args) {
-    int suma = 0;
+    int suma = 1;
     for (int contador = 1; contador <= 20; contador++) {
       if (contador % 2 != 0) {
         // System.out.println(contador);
-        suma += contador;
+        suma *= contador;
       }
     }
-    System.out.println("La suma es: " + suma);
+    System.out.println("El producto es: " + suma);
   }
 }
