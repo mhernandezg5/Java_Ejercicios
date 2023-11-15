df5eae2e5ad607f51906e262843422c7fc3c475e
unknown <myn0r>
2023-11-15 21:01:13 +0100

    Ejercicio 10

---
+++ Ciclos/Ejercicio_10.java
@@ -0,0 +1,11 @@
+/*
+ * Pedir 10 números y escribir la suma total
+ */
+package Ciclos;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio_10 {
+
+  public static void main(String[] args) {}
+}
