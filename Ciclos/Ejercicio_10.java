86545d07503e23003002014ff6c0736b2f1e45cb
unknown <myn0r>
2023-11-15 21:33:44 +0100

    Ejercicio10

--- Ciclos/Ejercicio_10.java
+++ Ciclos/Ejercicio_10.java
@@ -7,5 +7,14 @@ import javax.swing.JOptionPane;
 
 public class Ejercicio_10 {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int suma = 0;
+    for (int contador = 1; contador <= 10; contador++) {
+      int numero = Integer.parseInt(
+        JOptionPane.showInputDialog("Escriba un número")
+      );
+      suma = suma + numero;
+    }
+    JOptionPane.showMessageDialog(null, "La suma es: " + suma);
+  }
 }
