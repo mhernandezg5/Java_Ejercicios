639c1088cb275a46a8d9ba1f0f5c3b9beaf02a4d
unknown <myn0r>
2023-11-16 18:03:39 +0100

    Ejercicio 13

--- Ciclos/Ejercicio_13.java
+++ Ciclos/Ejercicio_13.java
@@ -11,20 +11,41 @@ import javax.swing.JOptionPane;
 public class Ejercicio_13 {
 
   public static void main(String[] args) {
-    int numero, contador, suma_enteros = 0, media_enteros;
+    int numero, contador, suma_positivos =
+      0, media_positivos, contador_positivos = 0, suma_negativos =
+      0, contador_negativos = 0, media_negativos, contador_ceros = 0;
     for (contador = 1; contador < 11; contador++) {
       numero =
         Integer.parseInt(JOptionPane.showInputDialog("Escribe un número"));
-      suma_enteros += numero;
+      if (numero > 0) {
+        contador_positivos++;
+        suma_positivos += numero;
+      } else if (numero < 0) {
+        contador_negativos++;
+        suma_negativos += numero;
+      } else if (numero == 0) {
+        contador_ceros++;
+      } else if (contador == 0) {
+        JOptionPane.showMessageDialog(null, "Error div 0 !");
+      }
     }
     //System.out.println(suma_enteros);
     // System.out.println(contador);
-    media_enteros = suma_enteros / (contador - 1);
+    media_positivos = suma_positivos / (contador - 1);
+    media_negativos = suma_negativos / (contador - 1);
 
     //Mostrar la media de los enteros
     JOptionPane.showMessageDialog(
       null,
-      "La media de los positivos es: " + media_enteros
+      "La media de los positivos es: " + media_positivos
+    );
+    JOptionPane.showMessageDialog(
+      null,
+      "La media de los negativos es: " + media_negativos
+    );
+    JOptionPane.showMessageDialog(
+      null,
+      "La cantidad de ceros es: " + contador_ceros
     );
   }
 }
