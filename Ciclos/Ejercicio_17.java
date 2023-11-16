d4ad941dc77008a89f778f2688884b7e40bc1462
unknown <myn0r>
2023-11-16 17:04:49 -0600

    Ejercicio 17

---
+++ Ciclos/Ejercicio_17.java
@@ -0,0 +1,15 @@
+/*
+ * Una empresa que se dedica a la venta de desinfectantes
+ * necesita un programa para gestionar las facturas.
+ * En cada factura figura: el código del artículo,
+ * la cantidad vendida en litros y el precio por litro.
+ * Se pide de 5 facturas introducidas: Facturación total,
+ * cantidad en litros vendidos del artículo 1 y cuantas
+ * facturas se emitieron de más de $600.
+ */
+package Ciclos;
+
+public class Ejercicio_17 {
+
+  public static void main(String[] args) {}
+}
