4b344594277a2b6ca5ba49caec352004bc176670
myn0r <myn0r@hotmail.com>
2023-11-21 11:48:12 -0600

    Ejercicio 1 - arreglos

--- Arreglos/Ejercicio_1.java
+++ Arreglos/Ejercicio_1.java
@@ -1,8 +1,19 @@
+/*
+ * Leer 5 números, guardarlos en un arreglo 
+ * y mostrarlos en el mismo orden introducido.
+ */
 package Arreglos;
+
+import javax.swing.JOptionPane;
 
 public class Ejercicio_1 {
   public static void main(String[] args) {
-
+    int[] numeros = new int[5];
+    for (int contador = 0; contador < numeros.length; contador++) {
+      numeros[contador] = Integer.parseInt(JOptionPane.showInputDialog("Escribe un número enter:"));
+    }
+    for (int iterador = 0; iterador < numeros.length; iterador++) {
+      System.out.println(iterador + 1 + ": " + numeros[iterador]);
+    }
   }
-
 }
