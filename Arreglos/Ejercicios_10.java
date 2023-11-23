ffe922dd0278637b2a7b8e8a73c72aebbbc7af07
myn0r <myn0r@hotmail.com>
2023-11-23 11:58:23 -0600

    .

--- Arreglos/Ejercicios_10.java
+++ Arreglos/Ejercicios_10.java
@@ -11,29 +11,30 @@ public class Ejercicios_10 {
     public static void main(String[] args) {
         int[] numeros = new int[10];
 
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("ARREGLO");
-        for (int contador = 0; contador < numeros.length; contador++) {
-            System.out.print((contador + 1) + ". Numero: ");
-            numeros[contador] = entrada.nextInt();
-        }
-        // Pedir el número de posiciones a desplazar
-        System.out.print("Ingrese la cantidad de posiciones a desplazar: ");
-        int n = entrada.nextInt();
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("ARREGLO");
+            for (int contador = 0; contador < numeros.length; contador++) {
+                System.out.print((contador + 1) + ". Numero: ");
+                numeros[contador] = entrada.nextInt();
+            }
+            // Pedir el número de posiciones a desplazar
+            System.out.print("Ingrese la cantidad de posiciones a desplazar: ");
+            int n = entrada.nextInt();
 
-        // Crear un nuevo arreglo para almacenar el resultado del desplazamiento
-        int[] numerosDesplazados = new int[numeros.length];
+            // Crear un nuevo arreglo para almacenar el resultado del desplazamiento
+            int[] numerosDesplazados = new int[numeros.length];
 
-        // Realizar el desplazamiento de los elementos
-        for (int i = 0; i < numeros.length; i++) {
-            int nuevaPosicion = (i + n) % numeros.length;
-            numerosDesplazados[nuevaPosicion] = numeros[i];
-        }
+            // Realizar el desplazamiento de los elementos
+            for (int i = 0; i < numeros.length; i++) {
+                int nuevaPosicion = (i + n) % numeros.length;
+                numerosDesplazados[nuevaPosicion] = numeros[i];
+            }
 
-        // Imprimir el arreglo con los elementos desplazados
-        System.out.println("Arreglo con elementos desplazados:");
-        for (int i : numerosDesplazados) {
-            System.out.print(i + " ");
+            // Imprimir el arreglo con los elementos desplazados
+            System.out.println("Arreglo con elementos desplazados:");
+            for (int i : numerosDesplazados) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
